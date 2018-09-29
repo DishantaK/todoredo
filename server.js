e6684d7b602aca3051b2e97b44a37925df5c49cc
shagnar <nykiakpatrick@gmail.com>
2018-09-29 07:50:25 -0400

    updated port var

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 // Defines a PORT for the server to listen for requests
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // Sets up our server to parse our request body for usage
 app.use(express.urlencoded({ extended: true }));
